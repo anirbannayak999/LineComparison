16e48c51ce6714d269320628b4a4397ccebaf0cd
“anirban <anirbannayak999@gmail.com>
2022-12-24 00:37:10 +0530

    code for equality of two lines

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -18,7 +18,27 @@ public class LineComparison {static int x1;
             int length1 = (int) Math.sqrt( (Math.pow((x1-x2),2) + Math.pow((y1-y2),2)) );
             System.out.println("Length of the line : " + length1);
 
+            x1 = 3;
+            y1 = 6;
+
+            x2 = 2;
+            y2 = -5;
+
+            int length2 = (int) Math.sqrt( (Math.pow((x1-x2),2) + Math.pow((y1-y2),2)) );
+            System.out.println("Length of the line : " + length2);
+
+            if(length1 > length2) {
+                System.out.println("Length 1 is grater than Length 2");
+            }else if(length1 < length2) {
+                System.out.println("Length 2 is greater than Length 1");
+            }else
+                System.out.println("Both Length are same");
+
+        }
+
+        }
 
 
-    }
-}
+
+
+
